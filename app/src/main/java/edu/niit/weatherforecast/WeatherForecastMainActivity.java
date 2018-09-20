81a0f1303c079cdc5efbb0b35786db69bd296858
nala1997 <1735776940@qq.com>
2018-09-20 16:41:54 +0800

    Initial commit

---
+++ app/src/main/java/edu/niit/weatherforecast/WeatherForecastMainActivity.java
@@ -0,0 +1,13 @@
+package edu.niit.weatherforecast;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class WeatherForecastMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_weather_forecast_main);
+    }
+}
